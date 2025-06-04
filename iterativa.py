147d45193f8f9be71dbc8a0807ee7f906d5665d3
ProyectCyber <108159807+Juancabarrera26@users.noreply.github.com>
2025-06-04 09:25:05 -0500

    Update iterativa.py

--- iterativa.py
+++ iterativa.py
@@ -6,10 +6,10 @@ def iterativa(f, t0, T, N, M, integrar):
 
     for n in range(1, N + 1):
         def funcion_coseno(t):
-            return f(t) * (np.cos(2 * np.pi * n * t / T))
+            return f(t) * np.cos(2 * np.pi * n * t / T)
 
         def funcion_seno(t):
-            return f(t) * (np.sin(2 * np.pi * n * t / T))
+            return f(t) * np.sin(2 * np.pi * n * t / T)
 
         an = (2 / T) * integrar(funcion_coseno, t0, t0 + T, M)
         bn = (2 / T) * integrar(funcion_seno, t0, t0 + T, M)
